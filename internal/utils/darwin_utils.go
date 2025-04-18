510973c85aa0415d3d3f946d28174bcbb9623782
Faipang <448253102@qq.com>
2025-04-18 15:43:50 +0800

    [*] fix /utils Fix cross-platform package compilation issues

---
+++ internal/utils/darwin_utils.go
@@ -0,0 +1,22 @@
+//go:build darwin
+
+package utils
+
+import "github.com/jaypipes/ghw"
+
+func GetMemoryInfo() (*MemoryInfo, error) {
+	memoryObj, err := ghw.Memory()
+	if err != nil {
+		return &MemoryInfo{}, err
+	}
+	memorySize := int(memoryObj.TotalUsableBytes / 1024 / 1024 / 1024)
+	memoryInfo = &MemoryInfo{
+		Size: memorySize,
+	}
+	return memoryInfo, nil
+}
+
+func GetSystemVersion() int {
+	systemVersion := 0
+	return systemVersion
+}
