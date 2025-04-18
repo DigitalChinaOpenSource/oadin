085cbca44a418a76e945cd65d3b166b7027a25ad
Faipang <448253102@qq.com>
2025-04-18 16:49:33 +0800

    [*] fix  /utils Fix getting memory size on Darwin/Linux

--- internal/utils/linux_utils.go
+++ internal/utils/linux_utils.go
@@ -2,14 +2,9 @@
 
 package utils
 
-import "github.com/jaypipes/ghw"
-
 func GetMemoryInfo() (*MemoryInfo, error) {
-	memoryObj, err := ghw.Memory()
-	if err != nil {
-		return &MemoryInfo{}, err
-	}
-	memorySize := int(memoryObj.TotalUsableBytes / 1024 / 1024 / 1024)
+	v, _ := mem.VirtualMemory()
+	memorySize := v.Total / 1024 / 1024 / 1024
 	memoryInfo := &MemoryInfo{
 		Size: memorySize,
 	}
