4df922e67be26200183d7b80c21736e6f0cf716e
刘畅 <liuchangaq@digitalchina.com>
2025-04-29 18:21:48 +0800

    feat: 封装 svg 图片组件，避免散落在各处

---
+++ src/components/icons/index.ts
@@ -0,0 +1,8 @@
+export { default as SearchIcon } from './search-icon';
+export { default as DownloadIcon } from './download-icon';
+export { default as CloudIcon } from './cloud-icon';
+export { default as LocalIcon } from './local-icon';
+export { default as SettingIcon } from './setting-icon';
+export { default as FailedIcon } from './failed-icon';
+export { default as LoadingIcon } from './loading-icon';
+export { default as DeleteIcon } from './delete-icon';
--- src/components/model-manage-tab/model-search/index.tsx
+++ src/components/model-manage-tab/model-search/index.tsx
@@ -1,5 +1,6 @@
 import styles from './index.module.scss'
 import { Input, Select } from 'antd';
+import { SearchIcon } from '../../icons';
 
 interface IModelTitleSearchProps {
   onModelSearch: (val: string) => void;
@@ -19,7 +20,7 @@ export default function ModelSearch(props: IModelTitleSearchProps) {
     <div className={styles.modelSearch}>
       <div className={styles.searchInput}>
         <Input
-          prefix={<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#808899" viewBox="0 0 256 256"><path d="M229.66,218.34l-50.07-50.06a88.11,88.11,0,1,0-11.31,11.31l50.06,50.07a8,8,0,0,0,11.32-11.32ZM40,112a72,72,0,1,1,72,72A72.08,72.08,0,0,1,40,112Z"></path></svg>}
+          prefix={<SearchIcon />}
           placeholder='请输入模型名称'
           value={modelSearchVal}
           onChange={(e) => onModelSearch(e.target.value)}
