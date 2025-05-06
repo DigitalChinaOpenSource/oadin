84aeb23444ad88fe3c29298da3a3f736ec4e184a
刘畅 <liuchangaq@digitalchina.com>
2025-05-06 10:47:49 +0800

    feat: 移除本地的 node server

--- src/components/main-layout/index.tsx
+++ src/components/main-layout/index.tsx
@@ -1,7 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import Sidebar from '../sidebar';
 import styles from './index.module.scss';
-// import DiskInfo from '../disk-info';
 
 const MainLayout = () => {
   return (
@@ -10,7 +9,6 @@ const MainLayout = () => {
       <div className={styles.content}>
         <Outlet />
       </div>
-      {/* <DiskInfo /> */}
     </div>
   );
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { spawn } from 'child_process';
 import path from 'path';
-
-const PORT = process.env.PORT || 33333;
-
-// 启动后端服务器的函数
-function startBackendServer() {
-  const server = spawn('node', ['src/server.js'], {
-    stdio: 'inherit',
-    shell: true,
-  });
-
-  // 当 Vite 服务器关闭时，也关闭后端服务器
-  process.on('exit', () => {
-    server.kill();
-  });
-}
 
 export default defineConfig({
   build: {
@@ -29,20 +13,5 @@ export default defineConfig({
   },
   plugins: [
     react(),
-    {
-      name: 'start-backend',
-      configureServer() {
-        startBackendServer();
-      },
-    },
   ],
-  server: {
-    proxy: {
-      '/api': {
-        target: `http://localhost:${PORT}`,
-        changeOrigin: true,
-        rewrite: (path) => path,
-      },
-    },
-  },
 });
