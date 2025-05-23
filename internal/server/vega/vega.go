82e9a7e98fd8793c84be6b010ab15c3b6a333027
yexinf <yexinf@digitalchina.com>
2025-05-23 14:45:05 +0800

    feat: add model desc

--- internal/server/vega/vega.go
+++ internal/server/vega/vega.go
@@ -1,11 +1,13 @@
 package vega
 
 import (
+	"byze/internal/api/dto"
 	"byze/internal/utils/client"
 	"context"
 	"fmt"
 	"net/http"
 	"net/url"
+	"strconv"
 )
 
 type VegaClient struct {
@@ -27,7 +29,7 @@ func NewVegaClient() *VegaClient {
 	}
 }
 
-func QueryCloudModelJson(hybridPolicy string) error {
+func QueryCloudModelJson(hybridPolicy string) ([]Model, error) {
 	c := NewVegaClient()
 	routerPath := fmt.Sprintf("/vega/%s/service", "0.1")
 
@@ -35,13 +37,62 @@ func QueryCloudModelJson(hybridPolicy string) error {
 		HybridPolicy: hybridPolicy,
 	}
 	resp := QueryCloudModelJsonRespond{}
+
 	err := c.Client.Do(context.Background(), http.MethodPost, routerPath, req, &resp)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	if resp.HTTPCode > 200 {
 		fmt.Println(resp.Message)
-		return fmt.Errorf(resp.Message)
+		return nil, fmt.Errorf(resp.Message)
 	}
-	return nil
+	return resp.Data, nil
+}
+
+type LocalSupportModelData struct {
+	OllamaId    string  `json:"id"`
+	Name        string  `json:"name"`
+	Avatar      string  `json:"avatar"`
+	Description string  `json:"description"`
+	Class       string  `json:"class"`
+	Flavor      string  `json:"provider"`
+	Size        string  `json:"size"`
+	ParamsSize  float32 `json:"params_size"`
+}
+
+var typeMapClass = map[string]string{
+	"chat":          "文本生成",
+	"embed":         "文本向量化",
+	"text_to_image": "文生图",
+}
+
+func GetRemoteModels(models []Model) (map[string][]dto.LocalSupportModelData, error) {
+	RemoteServiceMap := make(map[string][]dto.LocalSupportModelData)
+	for _, model := range models {
+		size := strconv.Itoa(model.FileSize)
+		class, ok := typeMapClass[model.Type]
+		if !ok {
+			class = "未知"
+		}
+		flavor := model.SupplierName
+		LocalSupportModel := dto.LocalSupportModelData{
+			OllamaId:    model.Id,
+			Name:        model.Name,
+			Avatar:      model.Avatar,
+			Description: model.Description,
+			Class:       class,
+			Flavor:      flavor,
+			Size:        size,
+			ParamsSize:  model.ParameterScale,
+		}
+		if _, ok := RemoteServiceMap[model.Type]; !ok {
+			RemoteServiceMap[model.Type] = []dto.LocalSupportModelData{
+				LocalSupportModel,
+			}
+		} else {
+			RemoteServiceMap[model.Type] = append(RemoteServiceMap[model.Type], LocalSupportModel)
+		}
+
+	}
+	return RemoteServiceMap, nil
 }
