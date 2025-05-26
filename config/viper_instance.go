f3cc30f2cb03120c20ea1d3494b1d63ba1ab98e1
Trayvon <zhucana@digitalchina.com>
2025-05-26 19:03:02 +0800

    feat: config

--- config/viper_instance.go
+++ config/viper_instance.go
@@ -14,17 +14,16 @@ func GetViperInstance() *viper.Viper {
 	case "local":
 		configFile = "./config/config-local.yaml"
 	case "dev":
-		configFile = "config-dev.yaml"
+		configFile = "./config/config-dev.yaml"
 	case "prod":
-		configFile = "config-prod.yaml"
+		configFile = "./config/config-prod.yaml"
 	default:
-		configFile = "config-prod.yaml"
+		configFile = "./config/config-prod.yaml"
 	}
 
 	//使用 viper
 	ViperInstance := viper.New()
 	ViperInstance.SetConfigFile(configFile)
-	ViperInstance.AddConfigPath("./config")
 	ViperInstance.SetConfigType("yaml")
 	if err := ViperInstance.ReadInConfig(); err != nil {
 		panic(fmt.Errorf("read config failed: %s \n", err))
