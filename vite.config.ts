3f12987612fc4efad23e87afc73631f448992836
刘畅 <liuchangaq@digitalchina.com>
2025-05-27 15:10:03 +0800

    fix: update

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  base: './',
   build: {
     outDir: 'dist',
   },
@@ -13,11 +14,14 @@ export default defineConfig({
   },
   plugins: [react()],
   server: {
+    host: '127.0.0.1', // 指定主机地址
+    port: 16677, // 指定端口号
+    strictPort: true, // 如果端口已被占用，则会直接退出而不是尝试下一个可用端口
     proxy: {
       '/byze/v0.2': {
         // target: 'http://127.0.0.1:16688',
-        // target: 'http://10.3.73.109:16688', // 雨浩的动态ip
-        target: 'http://10.3.74.111:16688', // 朱灿的本地ip
+        target: 'http://10.3.73.109:16688', // 雨浩的动态ip
+        // target: 'http://10.3.74.111:16688', // 朱灿的本地ip
         changeOrigin: true,
         configure: (proxy, options) => {
           proxy.on('proxyReq', (proxyReq, req, res) => {
