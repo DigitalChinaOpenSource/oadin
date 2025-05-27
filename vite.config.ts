0d3771c4e89289a263235620bec60576347ddaf5
刘畅 <liuchangaq@digitalchina.com>
2025-05-27 09:39:27 +0800

    fix: update

--- vite.config.ts
+++ vite.config.ts
@@ -16,8 +16,8 @@ export default defineConfig({
     proxy: {
       '/byze/v0.2': {
         // target: 'http://127.0.0.1:16688',
-        target: 'http://10.3.73.109:16688', // 雨浩的动态ip
-        // target: 'http://10.3.74.111:16688', // 朱灿的本地ip
+        // target: 'http://10.3.73.109:16688', // 雨浩的动态ip
+        target: 'http://10.3.74.111:16688', // 朱灿的本地ip
         changeOrigin: true,
         configure: (proxy, options) => {
           proxy.on('proxyReq', (proxyReq, req, res) => {
