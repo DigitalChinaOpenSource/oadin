2a506d92a63d97acb35865ef6324d950dc8258c9
刘畅 <liuchangaq@digitalchina.com>
2025-05-27 18:13:15 +0800

    fix: 调整端口号

--- vite.config.ts
+++ vite.config.ts
@@ -15,12 +15,12 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '127.0.0.1', // 指定主机地址
-    port: 16677, // 指定端口号
+    port: 16699, // 指定端口号
     strictPort: true, // 如果端口已被占用，则会直接退出而不是尝试下一个可用端口
     proxy: {
       '/byze/v0.2': {
-        // target: 'http://127.0.0.1:16688',
-        target: 'http://10.3.73.109:16688', // 雨浩的动态ip
+        target: 'http://127.0.0.1:16688',
+        // target: 'http://10.3.73.109:16688', // 雨浩的动态ip
         // target: 'http://10.3.74.111:16688', // 朱灿的本地ip
         changeOrigin: true,
         configure: (proxy, options) => {
@@ -37,8 +37,8 @@ export default defineConfig({
       //   changeOrigin: true,
       // },
       '/health': {
-        // target: 'http://127.0.0.1:16688',
-        target: 'http://10.3.73.109:16688', // 雨浩的动态ip
+        target: 'http://127.0.0.1:16688',
+        // target: 'http://10.3.73.109:16688', // 雨浩的动态ip
         changeOrigin: true,
         configure: (proxy, options) => {
           proxy.on('proxyReq', (proxyReq, req, res) => {
