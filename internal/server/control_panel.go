ccdaa906f5aae7a6e228e984b95e9ceba22c5c43
Faipang <448253102@qq.com>
2025-05-27 16:09:31 +0800

    [+] feat /internal control panel

---
+++ internal/server/control_panel.go
@@ -0,0 +1,291 @@
+package server
+
+import (
+	"byze/internal/api/dto"
+	"byze/internal/datastore"
+	"byze/internal/provider/template"
+	"byze/internal/schedule"
+	"byze/internal/types"
+	"byze/internal/utils"
+	"byze/internal/utils/bcode"
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func GetModelFilePath(ctx context.Context) (*dto.GetModelFilePathResponse, error) {
+	var defaultPath string
+	userDir, err := os.UserHomeDir()
+	if err != nil {
+		return nil, err
+	}
+	defaultPath = filepath.Join(userDir, ".ollama")
+	res := &dto.GetModelFilePathData{}
+	//switch systemName {
+	//case "darwin":
+	//	cmd = exec.Command("echo", "$OLLAMA_MODELS")
+	//case "linux":
+	//	cmd = exec.Command("echo", "$OLLAMA_MODELS")
+	//case "windows":
+	//	cmd = exec.Command("cmd", "/C", "echo", "%OLLAMA_MODELS%")
+	//}
+	//cmdRes, err := cmd.Output()
+	//if err != nil {
+	//	return nil, err
+	//}
+
+	//if string(cmdRes) == "" {
+	//	res.Path = defaultPath
+	//} else {
+	//	res.Path = string(cmdRes)
+	//}
+
+	value := os.Getenv("OLLAMA_MODELS")
+	if value != "" {
+		res.Path = value
+	} else {
+		res.Path = defaultPath
+	}
+
+	return &dto.GetModelFilePathResponse{
+		*bcode.ControlPanelCode,
+		res,
+	}, nil
+}
+
+func GetFilePathSize(ctx context.Context, req *dto.GetPathDiskSizeInfoRequest) (*dto.GetPathDiskSizeInfoResponse, error) {
+	res := &dto.GetPathDiskSizeInfoResponse{}
+	sizeInfo, err := utils.SystemDiskSize(req.Path)
+	if err != nil {
+		return nil, err
+	}
+	res.Data = sizeInfo
+	res.Bcode = *bcode.ControlPanelCode
+	return res, nil
+}
+
+func ModifyModelFilePath(ctx context.Context, req *dto.ModifyModelFilePathRequest) (*dto.ModifyModelFilePathResponse, error) {
+	status, err := utils.SamePartitionStatus(req.SourcePath, req.TargetPath)
+	if err != nil {
+		return nil, err
+	}
+	if status {
+		err = utils.CopyDir(req.SourcePath, req.TargetPath)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		sourcePathSize, err := utils.GetFilePathTotalSize(req.SourcePath)
+		if err != nil {
+			return nil, err
+		}
+		targetDiskSizeInfo, err := utils.SystemDiskSize(req.TargetPath)
+		if err != nil {
+			return nil, err
+		}
+		if int(sourcePathSize) > targetDiskSizeInfo.FreeSize {
+			return nil, errors.New("Target file path size is not enough")
+		}
+		err = utils.CopyDir(req.SourcePath, req.TargetPath)
+		if err != nil {
+			return nil, err
+		}
+	}
+	envInfo := &utils.EnvVariables{
+		Name:  "OLLAMA_MODELS",
+		Value: req.TargetPath,
+	}
+	err = utils.ModifySystemUserVariables(envInfo)
+	if err != nil {
+		err = os.RemoveAll(req.TargetPath)
+		if err != nil {
+			return nil, err
+		}
+		return nil, err
+	}
+	err = os.RemoveAll(req.SourcePath)
+	if err != nil {
+		return nil, err
+	}
+
+	res := &dto.ModifyModelFilePathResponse{}
+	res.Bcode = *bcode.ControlPanelCode
+	res.Data = struct{}{}
+	return res, nil
+
+}
+
+func GetModelList(ctx context.Context, request dto.GetModelListRequest) (*dto.RecommendModelResponse, error) {
+	ds := datastore.GetDefaultDatastore()
+	flavor := request.Flavor
+	source := request.ServiceSource
+	serviceModelList := make(map[string][]dto.RecommendModelData)
+	if request.ServiceSource == types.ServiceSourceLocal {
+		localOllamaModelMap := make(map[string]dto.LocalSupportModelData)
+		localOllamaServiceMap := make(map[string][]dto.LocalSupportModelData)
+		fileContent, err := template.FlavorTemplateFs.ReadFile("local_model.json")
+		if err != nil {
+			fmt.Printf("Read file failed: %v\n", err)
+			return nil, err
+		}
+		// parse struct
+		err = json.Unmarshal(fileContent, &localOllamaServiceMap)
+		if err != nil {
+			fmt.Printf("Parse JSON failed: %v\n", err)
+			return nil, err
+		}
+		for _, serviceInfo := range localOllamaServiceMap {
+			for _, model := range serviceInfo {
+				localOllamaModelMap[model.Name] = model
+			}
+		}
+		//
+		//var recommendModelParamsSize float32
+		recommendModel, err := RecommendModels()
+		if err != nil {
+			return &dto.RecommendModelResponse{Data: nil}, err
+		}
+		flavor = "ollama"
+		//service := "chat"
+		var resModelNameList []string
+
+		for modelService, modelInfo := range recommendModel {
+			providerServiceDefaultInfo := schedule.GetProviderServiceDefaultInfo(flavor, modelService)
+			parts := strings.SplitN(providerServiceDefaultInfo.Endpoints[0], " ", 2)
+			for _, model := range modelInfo {
+				localModelInfo := localOllamaModelMap[model.Name]
+				modelQuery := new(types.Model)
+				modelQuery.ModelName = strings.ToLower(model.Name)
+				modelQuery.ProviderName = fmt.Sprintf("%s_%s_%s", source, flavor, modelService)
+				canSelect := true
+				err := ds.Get(context.Background(), modelQuery)
+				if err != nil {
+					canSelect = false
+				}
+				if modelQuery.Status != "downloaded" {
+					canSelect = false
+				}
+				model.Service = modelService
+				model.Flavor = flavor
+				model.Method = parts[0]
+				model.Desc = localModelInfo.Description
+				model.Url = providerServiceDefaultInfo.RequestUrl
+				model.AuthType = providerServiceDefaultInfo.AuthType
+				model.IsRecommended = true
+				model.CanSelect = canSelect
+				model.ServiceProvider = fmt.Sprintf("%s_%s_%s", source, flavor, modelService)
+				model.Avatar = localModelInfo.Avatar
+				//model.Class = localModelInfo.Class
+				model.OllamaId = localModelInfo.OllamaId
+				serviceModelList[modelService] = append(serviceModelList[modelService], model)
+				resModelNameList = append(resModelNameList, model.Name)
+			}
+
+		}
+		for modelService, modelInfo := range localOllamaServiceMap {
+			providerServiceDefaultInfo := schedule.GetProviderServiceDefaultInfo(flavor, modelService)
+			if providerServiceDefaultInfo.Endpoints == nil {
+				continue
+			}
+			parts := strings.SplitN(providerServiceDefaultInfo.Endpoints[0], " ", 2)
+			for _, localModel := range modelInfo {
+				if !utils.Contains(resModelNameList, localModel.Name) {
+					modelQuery := new(types.Model)
+					modelQuery.ModelName = strings.ToLower(localModel.Name)
+					modelQuery.ProviderName = fmt.Sprintf("%s_%s_%s", source, flavor, modelService)
+					canSelect := true
+					err := ds.Get(context.Background(), modelQuery)
+					if err != nil {
+						canSelect = false
+					}
+					if modelQuery.Status != "downloaded" {
+						canSelect = false
+					}
+					model := new(dto.RecommendModelData)
+					model.Name = localModel.Name
+					model.Service = modelService
+					model.Flavor = flavor
+					model.Method = parts[0]
+					model.Desc = localModel.Description
+					model.Url = providerServiceDefaultInfo.RequestUrl
+					model.AuthType = providerServiceDefaultInfo.AuthType
+					model.IsRecommended = false
+					model.CanSelect = canSelect
+					model.ServiceProvider = fmt.Sprintf("%s_%s_%s", source, flavor, modelService)
+					model.Avatar = localModel.Avatar
+					//model.Class = localModel.Class
+					model.Size = localModel.Size
+					model.OllamaId = localModel.OllamaId
+					serviceModelList[modelService] = append(serviceModelList[modelService], *model)
+					resModelNameList = append(resModelNameList, model.Name)
+				}
+			}
+
+		}
+
+	} else {
+		RemoteServiceMap := make(map[string][]dto.LocalSupportModelData)
+		fileContent, err := template.FlavorTemplateFs.ReadFile("remote_model.json")
+		if err != nil {
+			fmt.Printf("Read file failed: %v\n", err)
+			return nil, err
+		}
+		// parse struct
+		err = json.Unmarshal(fileContent, &RemoteServiceMap)
+		if err != nil {
+			fmt.Printf("Parse JSON failed: %v\n", err)
+			return nil, err
+		}
+		for _, service := range types.SupportService {
+			if service == types.ServiceModels || service == types.ServiceGenerate {
+				continue
+			}
+			remoteModelInfoList := RemoteServiceMap[service]
+			providerServiceDefaultInfo := schedule.GetProviderServiceDefaultInfo(flavor, service)
+			parts := strings.SplitN(providerServiceDefaultInfo.Endpoints[0], " ", 2)
+			authFields := []string{"api_key"}
+			if providerServiceDefaultInfo.AuthType == types.AuthTypeToken {
+				authFields = []string{"secret_id", "secret_key"}
+			}
+			for _, model := range remoteModelInfoList {
+				if flavor != "" && model.Flavor != flavor {
+					continue
+				}
+				modelQuery := new(types.Model)
+				modelQuery.ModelName = model.Name
+				modelQuery.ProviderName = fmt.Sprintf("%s_%s_%s", source, flavor, service)
+				canSelect := true
+				err := ds.Get(context.Background(), modelQuery)
+				if err != nil {
+					canSelect = false
+				}
+				if modelQuery.Status != "downloaded" {
+					canSelect = false
+				}
+				modelData := dto.RecommendModelData{
+					Name:            model.Name,
+					Avatar:          model.Avatar,
+					Desc:            model.Description,
+					Service:         service,
+					Flavor:          flavor,
+					Method:          parts[0],
+					Url:             providerServiceDefaultInfo.RequestUrl,
+					AuthType:        providerServiceDefaultInfo.AuthType,
+					AuthFields:      authFields,
+					AuthApplyUrl:    providerServiceDefaultInfo.AuthApplyUrl,
+					ServiceProvider: fmt.Sprintf("%s_%s_%s", source, flavor, service),
+					CanSelect:       canSelect,
+				}
+				serviceModelList[service] = append(serviceModelList[service], modelData)
+			}
+		}
+	}
+	return &dto.RecommendModelResponse{
+		Bcode: *bcode.ModelCode,
+		Data:  serviceModelList,
+	}, nil
+}
---
+++ internal/types/json_datastore.go
@@ -0,0 +1,48 @@
+package types
+
+import "time"
+
+// SupportModel  table structure
+type SupportModel struct {
+	Id            string    `json:"id"`
+	OllamaId      string    `json:"Ollama_id"`
+	Name          string    `json:"name"`
+	Avatar        string    `json:"avatar"`
+	Description   string    `json:"description"`
+	Class         []string  `json:"class"`
+	Flavor        string    `json:"flavor"`
+	ApiFlavor     string    `json:"api_flavor"`
+	Size          string    `json:"size"`
+	ParamSize     float32   `json:"params_size"`
+	InputLength   int       `json:"input_length"`
+	OutputLength  int       `json:"output_length"`
+	ServiceSource string    `json:"service_source"`
+	ServiceName   string    `json:"service_name"`
+	CreatedAt     time.Time `gorm:"column:created_at;default:CURRENT_TIMESTAMP" json:"created_at"`
+	UpdatedAt     time.Time `gorm:"column:updated_at;default:CURRENT_TIMESTAMP" json:"updated_at"`
+}
+
+func (s *SupportModel) TableName() string {
+	return "support_model"
+}
+
+func (s *SupportModel) SetCreateTime(time time.Time) {
+	s.CreatedAt = time
+}
+
+func (s *SupportModel) SetUpdateTime(time time.Time) {
+	s.UpdatedAt = time
+}
+
+func (s *SupportModel) PrimaryKey() string {
+	return "name"
+}
+
+func (s *SupportModel) Index() map[string]interface{} {
+	index := make(map[string]interface{})
+	if s.Name != "" {
+		index["name"] = s.Name
+	}
+
+	return index
+}
