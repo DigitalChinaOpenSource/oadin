1a0b7d923f04d5160affd8c91db907e50ef72845
Faipang <448253102@qq.com>
2025-05-27 16:33:47 +0800

    [*] fix /internal control panel mac package

--- internal/utils/darwin_utils.go
+++ internal/utils/darwin_utils.go
@@ -42,7 +42,7 @@ func SamePartitionStatus(srcPath string, targetPath string) (bool, error) {
 	return stat1.Dev == stat2.Dev, nil
 }
 
-func ModifySystemUserVariables(envInfo EnvVariables) error {
+func ModifySystemUserVariables(envInfo *EnvVariables) error {
 	currentUser, err := user.Current()
 	if err != nil {
 		return err
