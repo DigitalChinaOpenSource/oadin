56f1c758a60fff93401098b3934021b24c43be02
Faipang <448253102@qq.com>
2025-05-30 10:41:11 +0800

    [*] fix  /tray system tray view logs

--- tray/utils/logging_windows.go
+++ tray/utils/logging_windows.go
@@ -7,31 +7,15 @@ import (
 	"fmt"
 	"log/slog"
 	"os/exec"
-	"runtime"
 	"syscall"
 )
 
 func ShowLogs() error {
-	var cmd *exec.Cmd
 
-	switch runtime.GOOS {
-	case "windows":
-		// Windows implementation (your existing code)
-		cmd_path := "c:\\Windows\\system32\\cmd.exe"
-		cmd = exec.Command(cmd_path, "/c", "start", config.GlobalByzeEnvironment.LogDir)
-		cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: false, CreationFlags: 0x08000000}
-
-	case "darwin": // macOS
-		// On macOS, we use 'open' command to open the directory in Finder
-		cmd = exec.Command("open", config.GlobalByzeEnvironment.LogDir)
-
-	case "linux":
-		// On Linux, we try common file managers (xdg-open works on most distributions)
-		cmd = exec.Command("xdg-open", config.GlobalByzeEnvironment.LogDir)
-
-	default:
-		return fmt.Errorf("unsupported platform: %s", runtime.GOOS)
-	}
+	// Windows implementation (your existing code)
+	cmd_path := "c:\\Windows\\system32\\cmd.exe"
+	cmd := exec.Command(cmd_path, "/c", "start", config.GlobalByzeEnvironment.LogDir)
+	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: false, CreationFlags: 0x08000000}
 
 	err := cmd.Start()
 	if err != nil {
