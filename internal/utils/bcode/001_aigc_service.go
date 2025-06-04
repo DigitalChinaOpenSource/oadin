00cb9b57965a4ff34d45bf4744fd7e97543a9081
ligjn <ligjn@digitalchina.com>
2025-06-04 11:26:17 +0800

    [*] fix: Modify the error code httpcode

--- internal/utils/bcode/001_aigc_service.go
+++ internal/utils/bcode/001_aigc_service.go
@@ -7,29 +7,29 @@ var (
 
 	ErrAIGCServiceBadRequest = NewBcode(http.StatusBadRequest, 10001, "bad request")
 
-	ErrAIGCServiceInitEnv = NewBcode(http.StatusBadRequest, 10002, "set env failed")
+	ErrAIGCServiceInitEnv = NewBcode(http.StatusInternalServerError, 10002, "set env failed")
 
-	ErrAIGCServiceInstallEngine = NewBcode(http.StatusBadRequest, 10003, "install model engine failed")
+	ErrAIGCServiceInstallEngine = NewBcode(http.StatusInternalServerError, 10003, "install model engine failed")
 
-	ErrAIGCServiceStartEngine = NewBcode(http.StatusBadRequest, 10004, "start model engine failed")
+	ErrAIGCServiceStartEngine = NewBcode(http.StatusInternalServerError, 10004, "start model engine failed")
 
-	ErrGetEngineModelList = NewBcode(http.StatusBadRequest, 10005, "get model list failed")
+	ErrGetEngineModelList = NewBcode(http.StatusInternalServerError, 10005, "get model list failed")
 
-	ErrEnginePullModel = NewBcode(http.StatusBadRequest, 10006, "pull model failed")
+	ErrEnginePullModel = NewBcode(http.StatusInternalServerError, 10006, "pull model failed")
 
-	ErrAIGCServiceAddProvider = NewBcode(http.StatusBadRequest, 10007, "provider insert db failed")
+	ErrAIGCServiceAddProvider = NewBcode(http.StatusInternalServerError, 10007, "provider insert db failed")
 
-	ErrAIGCServiceProviderIsExist = NewBcode(http.StatusBadRequest, 10009, "provider already exist")
+	ErrAIGCServiceProviderIsExist = NewBcode(http.StatusConflict, 10009, "provider already exist")
 
-	ErrServiceRecordNotFound = NewBcode(http.StatusBadRequest, 10011, "service not found")
+	ErrServiceRecordNotFound = NewBcode(http.StatusNotFound, 10011, "service not found")
 
-	ErrServiceUpdateFailed = NewBcode(http.StatusBadRequest, 10012, "service edit failed")
+	ErrServiceUpdateFailed = NewBcode(http.StatusInternalServerError, 10012, "service edit failed")
 
-	ErrAddModelService = NewBcode(http.StatusBadRequest, 10013, "add model service failed")
+	ErrAddModelService = NewBcode(http.StatusInternalServerError, 10013, "add model service failed")
 
-	ErrAIGCServiceVersionNotMatch = NewBcode(http.StatusBadRequest, 10014, "byze version not match")
+	ErrAIGCServiceVersionNotMatch = NewBcode(http.StatusUnprocessableEntity, 10014, "aog version not match")
 
-	ErrUnSupportAIGCService = NewBcode(http.StatusBadRequest, 10015, "unsupport byze service")
+	ErrUnSupportAIGCService = NewBcode(http.StatusBadRequest, 10015, "unsupport aog service")
 
 	ErrUnSupportHybridPolicy = NewBcode(http.StatusBadRequest, 10016, "unsupport hybrid policy")
 
