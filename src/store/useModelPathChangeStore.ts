09bb2c33592dda54ab44c9e2d04ea162e5059f3a
liuchang <liuchangaq@digitalchina.com>
2025-06-04 13:54:45 +0800

    fix: 添加路径迁移的状态管理

--- src/store/useModelPathChangeStore.ts
+++ src/store/useModelPathChangeStore.ts
@@ -1,14 +1,20 @@
 import { create } from 'zustand';
 
+type IStatus = 'init' | 'failed' | 'pending';
+
 interface ModelPathChangeState {
   isPathMigrating: boolean; // 标识模型存储路径是否正在迁移中
-  setMigratingStatus: (status: boolean) => void;
+  migratingStatus: IStatus; // 用于存储迁移状态
+  // 用于更新迁移状态
+  setIsPathMigrating: (status: boolean) => void;
+  setMigratingStatus: (status: IStatus) => void;
 }
 
 const useModelPathChangeStore = create<ModelPathChangeState>((set) => ({
   isPathMigrating: false,
-
-  setMigratingStatus: (status: boolean) => set({ isPathMigrating: status }),
+  migratingStatus: 'failed',
+  setMigratingStatus: (status: IStatus) => set({ migratingStatus: status }),
+  setIsPathMigrating: (status: boolean) => set({ isPathMigrating: status }),
 }));
 
 export default useModelPathChangeStore;
