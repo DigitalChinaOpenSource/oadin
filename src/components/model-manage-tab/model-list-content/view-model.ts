8c57df8419a1559d82c96a9cb9656698bfdf9ba2
liuchang <liuchangaq@digitalchina.com>
2025-06-06 13:40:48 +0800

    fix: update

--- src/components/model-manage-tab/model-list-content/view-model.ts
+++ src/components/model-manage-tab/model-list-content/view-model.ts
@@ -106,6 +106,7 @@ export function useViewModel(props: IModelListContent) {
   }, [modelSourceVal]);
 
   useEffect(() => {
+    if (!modelPath) return;
     onCheckPathSpace(modelPath);
   }, [modelPath]);
 
