b53a4a4fd7b67f9209276d9f54179ae67787d0e3
向秋怡 <xiangqy@digitalchina.com>
2025-06-10 10:55:11 +0800

    playground

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -22,6 +22,10 @@ type ModelServiceProvider interface {
 	ListModels(ctx context.Context) (*types.ListResponse, error)
 	GetConfig() *types.EngineRecommendConfig
 	GetVersion(ctx context.Context, resp *types.EngineVersionResponse) (*types.EngineVersionResponse, error)
+
+	Chat(ctx context.Context, req *types.ChatRequest) (*types.ChatResponse, error)
+	ChatStream(ctx context.Context, req *types.ChatRequest) (chan *types.ChatResponse, chan error)
+	GenerateEmbedding(ctx context.Context, req *types.EmbeddingRequest) (*types.EmbeddingResponse, error)
 }
 
 func GetModelEngine(engineName string) ModelServiceProvider {
