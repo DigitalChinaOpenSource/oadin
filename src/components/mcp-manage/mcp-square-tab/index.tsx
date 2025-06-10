055ecae2bf1ade86de30d8609804240e5d00ae8c
wangfajing <wangfjf@digitalchina.com>
2025-06-10 11:17:46 +0800

    feat: ✨️ 抽取MCP

--- src/components/mcp-manage/mcp-square-tab/index.tsx
+++ src/components/mcp-manage/mcp-square-tab/index.tsx
@@ -1,75 +1,53 @@
 import { Tooltip } from 'antd';
-import { Input } from 'antd';
 import { useViewModel } from './view-model';
 import expandSvg from '@/components/icons/expand.svg';
-import { MagnifyingGlassIcon } from '@phosphor-icons/react';
 import McpAdvanceFilter from '../mcp-advance-filter';
 import McpList from '../mcp-list';
-import styles from './index.module.scss';
-import { ITagsDataItem } from '@/components/mcp-manage/mcp-square-tab/types.ts';
+import commonStyles from '@/components/mcp-manage/mcp-common/index.module.scss';
+import { MyMcpTabProps } from '@/components/mcp-manage/my-mcp-tab';
+import { renderSearch, renderTitle, renderWarp } from '@/components/mcp-manage/mcp-common';
 
-export default function McpSquareTab() {
+export default function McpSquareTab(props: MyMcpTabProps) {
   const vm = useViewModel();
-
-  return (
-    <div className={styles.mcpSquareTab}>
-      {/* 列表区域 */}
-      <div className={styles.mcpSquareContent}>
-        <div className={styles.mcpTitle}>
-          <div className={styles.mcpTitleText}>
-            <span>MCP广场</span>
-            <span className={styles.mcpCount}>共 {vm.pagination.total} 个</span>
-          </div>
-          <div className={styles.searchInput}>
-            <Input
-              allowClear
-              placeholder="请输入 MCP 服务名称"
-              suffix={
-                <div
-                  className={styles.searchIcon}
-                  onClick={() => vm.onMcpInputSearch(vm.searchVal)}
-                >
-                  <MagnifyingGlassIcon
-                    width={16}
-                    height={16}
-                    fill="#808899"
-                  />
-                </div>
-              }
-              value={vm.searchVal}
-              onChange={(e) => vm.setSearchVal(e.target.value.trim())}
-              // onSearch={(value) => vm.onMcpInputSearch(value)}
-              onClear={() => {
-                vm.setSearchVal('');
-                vm.onMcpInputSearch('');
-              }}
-              onPressEnter={(e) => vm.onMcpInputSearch(vm.searchVal)}
-              style={{ width: 380 }}
-            />
-          </div>
-          {vm.collapsed && (
-            <Tooltip title="展开筛选">
-              <div
-                className={styles.expandIcon}
-                onClick={() => vm.setCollapsed(false)}
-              >
-                <img
-                  src={expandSvg}
-                  alt="折叠筛选面板"
-                />
-              </div>
-            </Tooltip>
-          )}
-        </div>
-        <McpList
-          mcpListData={vm.mcpListData}
-          onPageChange={vm.handlePageChange}
-          handelMcpCardClick={vm.handelMcpCardClick}
-          pagination={vm.pagination}
-          pageLoading={vm.mcpListLoading}
-        />
-      </div>
-      {/* 过滤器 */}
+  const { isDialog } = props;
+  return renderWarp({
+    isDialog,
+    titleNode: (
+      <>
+        {isDialog
+          ? null
+          : renderTitle({
+              title: 'MCP广场',
+              count: vm.pagination.total ?? 0,
+            })}
+        {renderSearch({
+          vm,
+        })}
+        {vm.collapsed && (
+          <Tooltip title="展开筛选">
+            <div
+              className={commonStyles.expandIcon}
+              onClick={() => vm.setCollapsed(false)}
+            >
+              <img
+                src={expandSvg}
+                alt="折叠筛选面板"
+              />
+            </div>
+          </Tooltip>
+        )}
+      </>
+    ),
+    childrenNode: (
+      <McpList
+        mcpListData={vm.mcpListData}
+        onPageChange={vm.handlePageChange}
+        handelMcpCardClick={vm.handelMcpCardClick}
+        pagination={vm.pagination}
+        pageLoading={vm.mcpListLoading}
+      />
+    ),
+    filterNode: (
       <McpAdvanceFilter
         collapsed={vm.collapsed}
         setCollapsed={vm.setCollapsed}
@@ -78,6 +56,6 @@ export default function McpSquareTab() {
         checkedValues={vm.checkedValues}
         handleTagsChange={vm.handleTagsChange}
       />
-    </div>
-  );
+    ),
+  });
 }
