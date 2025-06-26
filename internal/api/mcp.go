124bc2370112c1bde15bb9e9a1869c378f90ad20
Trayvon <zhucana@digitalchina.com>
2025-06-26 15:02:37 +0800

    feat: adjust the basic data of model

--- internal/api/mcp.go
+++ internal/api/mcp.go
@@ -89,16 +89,16 @@ func (t *ByzeCoreServer) DownloadMCP(c *gin.Context) {
 
 }
 
-func (t *ByzeCoreServer) Configuration(c *gin.Context) {
+func (t *ByzeCoreServer) AuthorizeMCP(c *gin.Context) {
 
 	id := c.Param("id")
 
-	config := map[string]string{}
+	auth := map[string]string{}
 
-	err := c.BindJSON(&config)
-	conStr, err := json.Marshal(config)
+	err := c.BindJSON(&auth)
+	authStr, err := json.Marshal(auth)
 
-	err = t.MCP.Configuration(c, id, string(conStr))
+	err = t.MCP.AuthorizeMCP(c, id, string(authStr))
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "资源不存在"})
 		return
