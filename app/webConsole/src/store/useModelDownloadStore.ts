8b6799086a78fb365daa21de7e8642d93f1df42a
liuchang <liuchangaq@digitalchina.com>
2025-06-27 11:27:49 +0800

    fix: siderbar 模型下载列表显示问题

--- app/webConsole/src/store/useModelDownloadStore.ts
+++ app/webConsole/src/store/useModelDownloadStore.ts
@@ -1,5 +1,4 @@
 import { create } from 'zustand';
-import { persist } from 'zustand/middleware';
 import { IModelDataItem } from '@/types';
 
 interface IModelDownloadStore {
@@ -9,57 +8,47 @@ interface IModelDownloadStore {
   setIsDownloadEmbed: (isDownloadEmbed: boolean) => void;
 }
 
-const useModelDownloadStore = create<IModelDownloadStore>()(
-  persist(
-    (set) => ({
-      downloadList: [],
-      setDownloadList: (list: any[] | ((currentList: any[]) => any[])) => {
-        if (typeof list === 'function') {
-          set((state: IModelDownloadStore) => {
-            const newList = list(state.downloadList);
-            // 处理词嵌入模型的avatar
-            const processedList = newList.map((item) => {
-              if (item.name === 'quentinz/bge-large-zh-v1.5:f16') {
-                return {
-                  ...item,
-                  avatar: 'http://120.232.136.73:31619/byzedev/model_avatar/BAAI.png',
-                };
-              }
-              return item;
-            });
+const useModelDownloadStore = create<IModelDownloadStore>((set, get) => ({
+  downloadList: [],
+  setDownloadList: (list: any[] | ((currentList: any[]) => any[])) => {
+    if (typeof list === 'function') {
+      set((state) => {
+        const newList = list(state.downloadList);
+        // 处理词嵌入模型的avatar
+        const processedList = newList.map((item) => {
+          if (item.name === 'quentinz/bge-large-zh-v1.5:f16') {
+            return {
+              ...item,
+              avatar: 'http://120.232.136.73:31619/byzedev/model_avatar/BAAI.png',
+            };
+          }
+          return item;
+        });
 
-            // 对象数组进行去重
-            const uniqueList = Array.from(new Map(processedList.map((item) => [JSON.stringify(item), item])).values());
-            return { downloadList: uniqueList };
-          });
-        } else {
-          const processedList = list.map((item) => {
-            // 判断是否为目标模型
-            if (item.name === 'quentinz/bge-large-zh-v1.5:f16') {
-              return {
-                ...item,
-                avatar: 'http://120.232.136.73:31619/byzedev/model_avatar/BAAI.png',
-              };
-            }
-            return item;
-          });
-
-          const uniqueList = Array.from(new Map(processedList.map((item) => [JSON.stringify(item), item])).values());
-          set({ downloadList: uniqueList });
+        // 对象数组进行去重
+        const uniqueList = Array.from(new Map(processedList.map((item) => [JSON.stringify(item), item])).values());
+        return { downloadList: uniqueList };
+      });
+    } else {
+      const processedList = list.map((item) => {
+        // 判断是否为目标模型
+        if (item.name === 'quentinz/bge-large-zh-v1.5:f16') {
+          return {
+            ...item,
+            avatar: 'http://120.232.136.73:31619/byzedev/model_avatar/BAAI.png',
+          };
         }
-      },
-      setIsDownloadEmbed: (isDownloadEmbed: boolean) => {
-        set({ isDownloadEmbed });
-      },
-    }),
-    {
-      name: 'model_download_store',
-      partialize: (state: any) => ({
-        isDownloadEmbed: state.isDownloadEmbed,
-        downloadList: state.downloadList,
-      }),
-    },
-  ),
-);
+        return item;
+      });
+
+      const uniqueList = Array.from(new Map(processedList.map((item) => [JSON.stringify(item), item])).values());
+      set({ downloadList: uniqueList });
+    }
+  },
+  isDownloadEmbed: false,
+  setIsDownloadEmbed: (isDownloadEmbed: boolean) => {
+    set({ isDownloadEmbed });
+  },
+}));
 
 export default useModelDownloadStore;
