df56f2dccdfe7731b25a500e0a42ee2539b937ac
liuchang <liuchangaq@digitalchina.com>
2025-06-30 19:25:24 +0800

    fix: update

--- app/webConsole/src/pages/model-chat/index.tsx
+++ app/webConsole/src/pages/model-chat/index.tsx
@@ -3,7 +3,7 @@ import ModelChecking from '@/components/model-checking';
 import styles from './index.module.scss';
 import useSelectedModelStore from '@/store/useSelectedModel';
 export default function ModelChat() {
-  const selectedModel = useSelectedModelStore.getState().selectedModel;
-
+  const { selectedModel } = useSelectedModelStore();
+  console.log('当前选中的模型', selectedModel);
   return <div className={styles.modelChat}>{selectedModel?.name ? <ChatContainer /> : <ModelChecking />}</div>;
 }
