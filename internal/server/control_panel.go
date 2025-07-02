cbd6dbd29740417d74773b460bbebf4da2ed16b4
Faipang <448253102@qq.com>
2025-07-02 15:51:38 +0800

    [*] fix ollama start not read env

--- internal/server/control_panel.go
+++ internal/server/control_panel.go
@@ -67,7 +67,8 @@ func ModifyModelFilePath(ctx context.Context, req *dto.ModifyModelFilePathReques
 	// Stop the engine before migration to avoid errors caused by processes still using the files.
 	engine := provider.GetModelEngine("ollama")
 	_ = engine.StopEngine()
-
+	engineConfig := engine.GetConfig()
+	engineConfig.StartStatus = 0
 	isSourceDirEmpty := utils.IsDirEmpty(req.SourcePath)
 	if !isSourceDirEmpty {
 		sourcePathSize, err := utils.GetFilePathTotalSize(req.SourcePath)
@@ -121,6 +122,7 @@ func ModifyModelFilePath(ctx context.Context, req *dto.ModifyModelFilePathReques
 	if err != nil {
 		return nil, err
 	}
+	engineConfig.StartStatus = 1
 
 	res := &dto.ModifyModelFilePathResponse{}
 	res.Bcode = *bcode.ControlPanelCode
