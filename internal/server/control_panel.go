c15578097c54ce8fa2bdb1ac5b22cba0c546e2c4
Faipang <448253102@qq.com>
2025-07-03 16:42:03 +0800

    [*] fix modify model path when model running

--- internal/server/control_panel.go
+++ internal/server/control_panel.go
@@ -48,6 +48,16 @@ func GetFilePathSize(ctx context.Context, req *dto.GetPathDiskSizeInfoRequest) (
 }
 
 func ModifyModelFilePath(ctx context.Context, req *dto.ModifyModelFilePathRequest) (*dto.ModifyModelFilePathResponse, error) {
+	engine := provider.GetModelEngine("ollama")
+	runningModels, _ := engine.GetRunModels(ctx)
+	if len(runningModels.Models) > 0 {
+		return &dto.ModifyModelFilePathResponse{}, bcode.ErrModelIsRunning
+	}
+	_ = engine.StopEngine()
+	engineConfig := engine.GetConfig()
+	if engineConfig.StartStatus == 0 {
+		return &dto.ModifyModelFilePathResponse{}, bcode.ErrModelEngineIsBeingOperatedOn
+	}
 	if req.TargetPath == req.SourcePath {
 		return &dto.ModifyModelFilePathResponse{}, bcode.ControlPanelPathStatusError
 	}
@@ -65,9 +75,7 @@ func ModifyModelFilePath(ctx context.Context, req *dto.ModifyModelFilePathReques
 	}
 
 	// Stop the engine before migration to avoid errors caused by processes still using the files.
-	engine := provider.GetModelEngine("ollama")
 	_ = engine.StopEngine()
-	engineConfig := engine.GetConfig()
 	engineConfig.StartStatus = 0
 	isSourceDirEmpty := utils.IsDirEmpty(req.SourcePath)
 	if !isSourceDirEmpty {
