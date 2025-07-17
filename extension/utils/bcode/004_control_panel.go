cab4d693a5a42a5d3f5fbaebc847e25e804f0430
yexinf <yexinf@digitalchina.com>
2025-07-17 18:53:43 +0800

    code

--- extension/utils/bcode/004_control_panel.go
+++ extension/utils/bcode/004_control_panel.go
@@ -5,20 +5,20 @@ import (
 )
 
 var (
-	ControlPanelCode = NewBcode(http.StatusOK, 40000, "control panel interface call success")
+	   ControlPanelCode = NewBcode(http.StatusOK, 40000, "control panel interface call success")
 
-	ControlPanelPathCheckError = NewBcode(http.StatusBadRequest, 40001, "path is not correct or not exists, please check!")
+	   ControlPanelPathCheckError = NewBcode(http.StatusBadRequest, 40001, "path is not correct or not exists, please check!")
 
-	ControlPanelPathSizeError = NewBcode(http.StatusBadRequest, 40002, "path size is not enough, please modify!")
+	   ControlPanelPathSizeError = NewBcode(http.StatusBadRequest, 40002, "path size is not enough, please modify!")
 
-	ControlPanelPathStatusError = NewBcode(http.StatusBadRequest, 40003, "path is not empty or same as the source path, please check!")
+	   ControlPanelPathStatusError = NewBcode(http.StatusBadRequest, 40003, "path is not empty or same as the source path, please check!")
 
-	ControlPanelCopyDirError = NewBcode(http.StatusInternalServerError, 40004, "migrate file failed, please retry!")
+	   ControlPanelCopyDirError = NewBcode(http.StatusInternalServerError, 40004, "migrate file failed, please retry!")
 
-	ControlPanelPathPermissionError = NewBcode(http.StatusForbidden, 40005, "path permission denied, please check!")
+	   ControlPanelPathPermissionError = NewBcode(http.StatusForbidden, 40005, "path permission denied, please check!")
 
-	// ControlPanelAddMcpError mcp 添加失败
-	ControlPanelAddMcpError = NewBcode(http.StatusInternalServerError, 40006, "mcp add failed, please retry!")
+	   // ControlPanelAddMcpError mcp 添加失败
+	   ControlPanelAddMcpError = NewBcode(http.StatusInternalServerError, 40006, "mcp add failed, please retry!")
 
-	ControlPanelSystemError = NewBcode(http.StatusInternalServerError, 40007, "system error, please retry!")
+	   ControlPanelSystemError = NewBcode(http.StatusInternalServerError, 40007, "system error, please retry!")
 )
