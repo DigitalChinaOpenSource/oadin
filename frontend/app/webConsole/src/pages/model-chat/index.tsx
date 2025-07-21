68ebc9751ea140c576b1079477c663659308e839
liuchang <liuchangaq@digitalchina.com>
2025-07-21 14:55:38 +0800

    fix: remove fe

--- frontend/app/webConsole/src/pages/model-chat/index.tsx
+++
@@ -1,9 +0,0 @@
-import ChatContainer from '@/components/chat-container';
-import ModelChecking from '@/components/model-checking';
-import styles from './index.module.scss';
-import useSelectedModelStore from '@/store/useSelectedModel';
-export default function ModelChat() {
-  const { selectedModel } = useSelectedModelStore();
-  console.log('当前选中的模型', selectedModel);
-  return <div className={styles.modelChat}>{selectedModel?.name ? <ChatContainer /> : <ModelChecking />}</div>;
-}
