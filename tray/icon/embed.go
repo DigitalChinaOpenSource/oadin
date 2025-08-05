f6a4a680e91c3cb4ef57bae3ef344d9646edcbc9
向秋怡 <xiangqy@digitalchina.com>
2025-08-05 17:33:24 +0800

    feat: add resource files for Oadin Tray Application including icon and version info

--- tray/icon/embed.go
+++ tray/icon/embed.go
@@ -9,6 +9,7 @@ import (
 //go:embed *.png *.ico
 var TrayIconFS embed.FS
 
+// DebugListFiles 列出所有嵌入的文件（调试用）
 func DebugListFiles() {
 	fmt.Println("Embedded icon files:")
 	fs.WalkDir(TrayIconFS, ".", func(path string, d fs.DirEntry, err error) error {
