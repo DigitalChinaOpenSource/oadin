515340ad78e6b0d507640e77c0774f5cd72fe81b
yexinf <yexinf@digitalchina.com>
2025-08-21 20:06:18 +0800

    code

--- installer/win/start-oadin.bat
+++ installer/win/start-oadin.bat
@@ -1,7 +1,6 @@
 @echo off
-set "USER_HOME=%USERPROFILE%"
 set "OADIN_HOME=%ProgramFiles%\Oadin"
 set "PATH=%OADIN_HOME%;%PATH%"
 
 REM 使用 start 命令独立启动 oadin，不依赖父进程
-oadin server start -d > "%USER_HOME%\Oadin\oadin-server.log" 2>&1
+oadin server start -d > "%OADIN_HOME%\oadin-server.log" 2>&1
