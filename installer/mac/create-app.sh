25af356214bff003d672dbf17b02e06ed02c7712
Faipang <448253102@qq.com>
2025-10-14 21:42:26 +0800

    [*] fix /tray mac permission

--- installer/mac/create-app.sh
+++ installer/mac/create-app.sh
@@ -32,7 +32,7 @@ fi
 # 复制可执行文件
 cp $(pwd)/oadin "${RESOURCES_DIR}/"
 cp $(pwd)/oadin-app "${MACOS_DIR}/"
-cp oadin-launcher.sh "${MACOS_DIR}/"
+cp $(pwd)/installer/mac/oadin-launcher.sh "${MACOS_DIR}/"
 
 # 复制应用图标（如果存在）
 if [ -f "tray/icon/oadin-icon.icns" ]; then
