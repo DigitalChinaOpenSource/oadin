65be968dac160898e435911d18287c3449c87147
朱灿 <zhucana@digitalchina.com>
2025-11-06 15:51:50 +0800

    feat: update Windows DLL dependency URLs to point to the new vc_redist executables
    Signed-off-by: 朱灿 <zhucana@digitalchina.com>

--- internal/provider/engine/ollama.go
+++ internal/provider/engine/ollama.go
@@ -57,8 +57,8 @@ const (
 	WindowsAMDURL           = constants.BaseDownloadURL + constants.UrlDirPathWindows + "/ollama-windows-amd64-rocm.zip"
 	WindowsIntelArcURL      = constants.BaseDownloadURL + constants.UrlDirPathWindows + "/ipex-llm-ollama.zip"
 	WindowsBaseURL          = constants.BaseDownloadURL + constants.UrlDirPathWindows + "/ollama-windows-amd64-base.zip"
-	WindowsDDLDependsX64URL = constants.OssURL + "/" + constants.AppName + constants.UrlDirPathWindows + "/dependency/VcRuntime_x64.zip"
-	WindowsDDLDependsX86URL = constants.OssURL + "/" + constants.AppName + constants.UrlDirPathWindows + "/dependency/VcRuntime_x86.zip"
+	WindowsDDLDependsX64URL = constants.OssURL + "/" + constants.AppName + constants.UrlDirPathWindows + "/dependency/vc_redist.x64.exe"
+	WindowsDDLDependsX86URL = constants.OssURL + "/" + constants.AppName + constants.UrlDirPathWindows + "/dependency/vc_redist.x86.exe"
 
 	// AMD GPU ROCM download URL
 	WindowsAMD_780M  = constants.OssURL + "/" + constants.AppName + constants.UrlDirPathWindows + "/ollama-windows-amd64-rocm-780M.zip"
