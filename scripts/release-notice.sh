80c8a19dbcd6085f00106c41fe39456640039af7
yexinf <yexinf@digitalchina.com>
2025-12-16 09:58:52 +0800

    feat: 飞书推送链接问题修复

--- scripts/release-notice.sh
+++ scripts/release-notice.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
 # 判断环境和下载路径
-if [[ "$ENV" == "development" ]]; then
-  MAC_URL="http://$NEXUS_HOST_PORT/repository/raw-hosted/intel-ai-pc/oadin/dist/mac/$MAC_FILE_NAME"
-  WIN_URL="http://$NEXUS_HOST_PORT/repository/raw-hosted/intel-ai-pc/oadin/dist/win/$WIN_FILE_NAME"
+# if [[ "$ENV" == "development" ]]; then
+#   MAC_URL="http://$NEXUS_HOST_PORT/repository/raw-hosted/intel-ai-pc/oadin/dist/mac/$MAC_FILE_NAME"
+#   WIN_URL="http://$NEXUS_HOST_PORT/repository/raw-hosted/intel-ai-pc/oadin/dist/win/$WIN_FILE_NAME"
+# else
+#   MAC_URL="https://oss-aipc.dcclouds.com/oadin/dist/macos/$MAC_FILE_NAME"
+#   WIN_URL="https://oss-aipc.dcclouds.com/oadin/dist/windows/$WIN_FILE_NAME"
+# fi
+
+# 如果$MAC_FILE_NAME和$WIN_FILE_NAME都包含test，则使用测试地址
+if [[ "$MAC_FILE_NAME" == *"test"* && "$WIN_FILE_NAME" == *"test"* ]]; then
+  MAC_URL="http://10.3.70.145:32018/repository/raw-hosted/intel-ai-pc/oadin/dist/mac/$MAC_FILE_NAME"
+  WIN_URL="http://10.3.70.145:32018/repository/raw-hosted/intel-ai-pc/oadin/dist/win/$WIN_FILE_NAME"
 else
   MAC_URL="https://oss-aipc.dcclouds.com/oadin/dist/macos/$MAC_FILE_NAME"
   WIN_URL="https://oss-aipc.dcclouds.com/oadin/dist/windows/$WIN_FILE_NAME"
